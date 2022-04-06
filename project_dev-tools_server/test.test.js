78104e86e60d23a9b68fcfcef52cd71c3037c078
Sakon Teerasuwonajak <flukgoza@gmail.com>
2022-04-06 21:23:18 +0700

    update api test

--- project_dev-tools_server/test.test.js
+++ project_dev-tools_server/test.test.js
@@ -3,33 +3,33 @@ const axios = require("axios");
 describe("Test get product API", () => {
   jest.setTimeout(10000)
   it("Get all tasks", async () => {
-    const result = await axios.get("http://localhost:3000/tasks");
+    const result = await axios.get("http://159.223.32.22:3000/tasks");
     expect(result.status).toEqual(200);
 
 
   });
   jest.setTimeout(10000)
   it("Post a task", async () => {
-    const result = await axios.post("http://localhost:3000/tasks");
+    const result = await axios.post("http://159.223.32.22:3000/tasks");
     expect(result.status).toEqual(200);
 
 
   });
   jest.setTimeout(10000)
   it("Read a task", async () => {
-    const result = await axios.get("http://localhost:3000/tasks/0");
+    const result = await axios.get("http://159.223.32.22:3000/tasks/0");
     expect(result.status).toEqual(200);
 
   });
   jest.setTimeout(10000)
   it("Update a task", async () => {
-    const result = await axios.put("http://localhost:3000/tasks/0");
+    const result = await axios.put("http://159.223.32.22:3000/tasks/0");
     expect(result.status).toEqual(200);
 
   });
   jest.setTimeout(10000)
   it("Delete a task", async () => {
-    const result = await axios.delete("http://localhost:3000/tasks/0");
+    const result = await axios.delete("http://159.223.32.22:3000/tasks/0");
     expect(result.status).toEqual(200);
 
   });
